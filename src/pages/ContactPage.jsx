dd59046f8e7efff86780885fbb75a0d9b49a3d10
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2025-04-05 17:04:20 +0100

    working on responsiveness of landing and contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -9,12 +9,12 @@ function ContactPage() {
       <section className="contact-section">
         <div className="container">
           <div className="row">
-            <div className="col-12 col-md-5 d-flex align-items-center mb-5">
+            <div className="col-12 col-md-12 col-lg-5 d-flex align-items-center mb-5">
               <div className="contact-form-wrapper">
                 <h1>
                   Get in <span className="blue-text">Touch</span>
                 </h1>
-                <p>
+                <p className="mb-3">
                   Have questions or need more information? We are here to help!
                   Whether you are looking to start your coding journey, need
                   guidance on our courses, or want to collaborate, feel free to
@@ -24,20 +24,20 @@ function ContactPage() {
                   <div className="mb-3">
                     <input
                       type="text"
-                      className="form-control form-control-lg shadow-none"
+                      className="form-control form-control-lg shadow-none w-100"
                       placeholder="Name *"
                     />
                   </div>
                   <div className="mb-3">
                     <input
                       type="email"
-                      className="form-control form-control-lg shadow-none"
+                      className="form-control form-control-lg shadow-none w-100"
                       placeholder="Email Address *"
                     />
                   </div>
                   <div class="mb-3">
                     <textarea
-                      className="form-control shadow-none"
+                      className="form-control shadow-none w-100"
                       id="exampleFormControlTextarea1"
                       placeholder="Message here..."
                       rows="5"
@@ -65,8 +65,8 @@ function ContactPage() {
                 </div>
               </div>
             </div>
-            <div className="d-sm-none d-md-grid col-1"></div>
-            <div className="col-12 col-md-6 mb-5">
+            <div className="col-1"></div>
+            <div className="col-12 col-md-12 col-lg-5 mb-5">
               <div className="location-wrapper">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.9407898168924!2d3.3269175746362856!3d7.132845792871152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103a4c7ac5685e43%3A0xee015cf77eaef13c!2s19%20Onikoko%20Rd%2C%20Panseke%2C%20Abeokuta%20111102%2C%20Ogun%20State!5e0!3m2!1sen!2sng!4v1743860388204!5m2!1sen!2sng"
