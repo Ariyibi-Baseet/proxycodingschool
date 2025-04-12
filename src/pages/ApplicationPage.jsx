f5ee44368b484def1860d67c36057917b724b4d6
SogelolaMoriojuoluwa <sogelolagrace@gmail.com>
2025-04-12 23:02:28 +0100

    creating an application page

--- src/pages/ApplicationPage.jsx
+++ src/pages/ApplicationPage.jsx
@@ -1,7 +1,181 @@
+import ProxyImage from '../assets/images/proxyimg.png'
+import {useForm} from "react-hook-form"
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import "../assets/css/Application.css"
+
+const courseData = {
+  "Full-stack Web Development": { month: "6 Months", price: "₦600,000",discountedprice:"₦450,000", time: "Tuesday's 12:00-3:00 PM , Friday's 9:00am-12:00pm" },
+  "Frontend Web Development": { month: "3 Months", price: "₦400,000",discountedprice: "₦220,000", time: "Tuesday's 12:00-3:00 PM, Friday's 9:00 AM -12:00 PM" },
+  "Cyber-Security": { month: "2 Months", price: "₦400,000",discountedprice: "₦250,000", time: "Monday's 9:00am-12:00pm, Wednesday's 9:00am-12:00pm" },
+  "Data Analytics": { month: "3 Months", price: "₦400,000",discountedprice: "₦250,000", time: "Tuesday's 9:00am-12:00pm, Thurdays 9:00am-12:00pm" },
+  "UIUX Design": { month:  "2 Months", price: "₦250,000",discountedprice: "₦180,000", time: "Tuesday's & Thursday's, 10AM-1PM" },
+  "Desktop Publishing": { month: "2 Months", price: "₦100,000",discountedprice: "₦80,000", time: "Monday's 12:00-3:00pm, Wednesday's 12:00-3:00pm, Friday's 12:00pm-3:00pm" },
+  "digital Marketing": { month: "3 Months", price: "₦300.000",discountedprice: "₦180,000", time: "Monday's 9:00am-12:00pm, Thursday's 9:00am-12:00pm" },
+  "Certified Ethical Hacking": { month: "6 Months", price: "₦950,000",discountedprice: "₦750,000", time: "Wednesday's & Friday's, 9am to 12PM" },
+  "Graphics Design & Branding": { month: "3 Months", price: "₦250,000",discountedprice: "₦160,000", time: "Tuesday's 9:00am-12:00pm, Thursday's 12:00pm-3:00pm"},
+  "Software Development & Mobile Apps": { month: "6 Months", price: "₦950,000",discountedprice: "₦650,000", time: "Tuesday's & Thursday's, 9:00am-12:00pm" },
+  "Python Programming": { month: "2 Months", price: "₦350,000",discountedprice: "₦250,0000", time: "Wednesday 9:-12PM & Weekdays 12pm-4pm" },
+  "Computer Engineering": { month: "3-6 Months", price: "₦350,000",discountedprice: "₦250,000", time: "Mondays , Wednesday &  Friday's, 10AM-5PM" },
+  "Computer Aided - Design": { month: "3 Months", price: "₦450,000",discountedprice: "₦280,000", time: "Saturday's, 12:000PM & 4:00PM" },
+};
+
 function ApplicationPage() {
+  const { register, handleSubmit,watch,setValue, formState:{ errors}} = useForm();
+
+  const selectedCourse = watch("course")
+
+  useEffect(() => {
+    if (selectedCourse && courseData[selectedCourse]) {
+      const { month, price, time, discountedprice } = courseData[selectedCourse];
+      setValue("month", month);
+      setValue("price", price);
+      setValue("time", time);
+      setValue("discountedprice", discountedprice);
+    }
+  }, [selectedCourse, setValue]);
+
+
+  const onSubmit = (data) => {
+    alert('good to go');
+  }
   return (
     <>
-      <h1>Under construction</h1>
+      <div className="container-fluid">
+      <div className='apply-body d-flex flex-column flex-lg-row min-vh-100'>
+      <div className="apply-body-content d-lg-flex d-none align-items-center justify-content-center ">
+        <div>
+          <div className='proxy-img d-flex justify-content-center'>
+          <img src={ProxyImage} alt=""/>
+          </div>
+        
+         <p className='text-center text-white intro1'>Welcome to proxy Coding School</p>
+         <p className="intro2 text-center text-white"> Application Form</p> 
+         <p className='text-center text-white intro3'>For school enquiry: 08039209990, 08039209990</p>
+        </div>
+       
+      </div>
+
+
+      <div className='form p-5 d-flex flex-column justify-content-center'>
+      <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+            <Link className='' to='/'>
+              <li className="breadcrumb-item">Home</li>
+              </Link>
+            
+              
+              
+            </ol>
+          </nav>
+        <div className='form-header'>
+        <h1>Application Form</h1>
+        <p>Kindly fill your details to start registration process</p>
+        </div>
+        
+
+        <form onSubmit={handleSubmit(onSubmit)} className="row g-3" autocomplete="off">
+   <div className="col-md-6">
+    <label for="firstname" className="form-label">First Name</label>
+    <input type="text" className="form-control" id="firstname shadow-none" {...register('firstname', {
+            required: 'Name is required',
+            minLength: {
+              value: 2,
+              message: 'Name must be at least 2 characters long'
+            },
+            pattern: {
+              value: /^[A-Za-z\s]+$/,
+              message: 'Name can only contain letters and spaces'
+            }
+          })}/>
+    {errors.firstname && <span className='text-danger'>{errors.firstname.message}</span>}
+    </div>
+
+    <div className="col-md-6">
+    <label for="name" className="form-label">Last Name</label>
+    <input type="text" className="form-control" id="lastname" {...register('lastname', {
+            required: 'Name is required',
+            minLength: {
+              value: 2,
+              message: 'Name must be at least 2 characters long'
+            },
+            pattern: {
+              value: /^[A-Za-z\s]+$/,
+              message: 'Name can only contain letters and spaces'
+            }
+          })}/>
+    {errors.lastname && <span className='text-danger'>{errors.lastname.message}</span>}
+    </div>
+
+    <div className="col-12">
+    <label for="phone-no" className="form-label">Phone Number</label>
+    <input type="tel" className="form-control" id="phone-no"  {...register('tel', {
+            required: 'Enter phone number',
+            pattern: {
+              value: /^[0-9]{10,15}$/,
+              message: 'Invalid Phone number'
+            }
+          })}/>
+           {errors.tel && <span className='text-danger'>{errors.tel.message}</span>}
+    </div>
+
+    <div className="col-12">
+    <label for="email" className="form-label">Email Address</label>
+    <input type="email" className="form-control" id="email" {...register('email', {
+      required: 'Enter email' ,
+      pattern: {
+        value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message:'Invalid Email'
+      }
+      
+       
+    })}/>
+      {errors.email && <span className='text-danger'>{errors.email.message}</span>}
+    </div>
+  
+    <div className="col-12">
+    <label for="select" className="form-label">Course Intrest</label> 
+    <select id="select" className="form-select"  {...register("course", { required: "Please select a course", 
+    })}>
+       
+      <option value="">Select a course</option>
+      {Object.keys(courseData).sort().map((course) => (
+            <option key={course} value={course}>{course}</option>
+          ))}
+              
+    </select>
+    {errors.course && (
+    <span className="text-danger">{errors.course.message}</span>
+  )}
+        </div>
+    <div className="col-md-6">
+    <input type="text" className="form-control" id="month" 
+    placeholder='Months'{...register("month")}readOnly/>
+  </div>
+  <div className="col-md-6">
+    <input type="text" className="form-control" id="time" placeholder="Time"
+          {...register("time")}
+          readOnly/>
+  </div>
+  <div className="col-md-6">
+      <input className="form-control" type="text" id="price"  placeholder="Price"
+          {...register("price")}
+          readOnly/>
+  </div>
+  <div className="col-md-6">
+      <input className="form-control" type="text" id="discountedprice"  placeholder="Discounted price"
+          {...register("discountedprice")}
+          readOnly/>
+  </div>
+  
+  <div className="d-grid col-12 mx-auto m-4">
+    <button type="submit" className="btn btn-primary">Apply Now</button>
+  </div>
+</form>
+      </div>
+      </div>
+      
+    </div>
     </>
   );
 }
